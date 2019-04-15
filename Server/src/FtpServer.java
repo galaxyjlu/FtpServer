333f417c99cb72fc7ef9a7710c7d0c93de68ebe2
GalaxyXL <galaxyxl@live.com>
2019-04-15 17:28:29 +0800

    Initial the project

---
+++ Server/src/FtpServer.java
@@ -0,0 +1,7 @@
+/**
+ * Author: GalaxyXL
+ * DateCteate in 2019-04-15 17:24
+ */
+
+public class FtpServer implements FtpServerRepository {
+}
---
+++ Server/src/FtpServerRepository.java
@@ -0,0 +1,7 @@
+/**
+ * Author: GalaxyXL
+ * DateCteate in 2019-04-15 17:23
+ */
+
+interface FtpServerRepository {
+}
