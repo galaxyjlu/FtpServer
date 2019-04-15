6c33449b6e5f8e1ef2226aa82749ff400a0ff2f9
GalaxyXL <galaxyxl@live.com>
2019-04-15 22:51:08 +0800

    Start Project

--- Server/src/FtpServer.java
+++ Server/src/FtpServer.java
@@ -1,7 +1,25 @@
+import java.net.ServerSocket;
+
 /**
  * Author: GalaxyXL
  * DateCteate in 2019-04-15 17:24
  */
 
-public class FtpServer implements FtpServerRepository {
+public class FtpServer implements FtpServerRepository, Runnable {
+    ServerSocket socket;
+
+    private FtpServer(){
+
+    }
+
+    private static FtpServer ftpServer = new FtpServer();
+
+    public static FtpServerRepository getInstance() {
+        return ftpServer;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
--- Server/src/FtpServerRepository.java
+++ Server/src/FtpServerRepository.java
@@ -1,7 +1,11 @@
+import java.net.ServerSocket;
+
 /**
  * Author: GalaxyXL
  * DateCteate in 2019-04-15 17:23
  */
 
 interface FtpServerRepository {
+    
+
 }
